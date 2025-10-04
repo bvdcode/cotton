59aa3e0ce24eeae36e6d85eaf6d3590229d2e4b9
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-10-04 00:41:54 -0700

    Add PostgreSQL support and database integration
    - Added `EasyExtensions.EntityFrameworkCore` and `EasyExtensions.EntityFrameworkCore.Npgsql` NuGet packages.
    - Configured `CottonDbContext` for PostgreSQL in `Program.cs`.
    - Updated `appsettings.Development.json` with database settings.
    - Created `FileDescriptor` entity and integrated it into `CottonDbContext`.
    - Streamlined application pipeline by removing unused middleware.

--- Sources/Cotton.Server/Program.cs
+++ Sources/Cotton.Server/Program.cs
@@ -1,3 +1,5 @@
+using Cotton.Server.Database;
+using EasyExtensions.EntityFrameworkCore.Npgsql.Extensions;
 
 namespace Cotton.Server
 {
@@ -6,33 +8,21 @@ namespace Cotton.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddOpenApi()
+                .AddPostgresDbContext<CottonDbContext>(builder.Configuration);
 
             var app = builder.Build();
-
             app.UseDefaultFiles();
             app.MapStaticAssets();
-
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.MapFallbackToFile("/index.html");
-
             app.Run();
         }
     }
