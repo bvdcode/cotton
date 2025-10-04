27a3060e245ed6eeb2565a4485c0320a64d730e1
Vadim Belov <git@belov.us>
2025-10-04 02:10:56 -0700

    Refactor database models: Replace File* with Blob*
    Replaced `FileDescriptor` and `FileChunk` entities with `Blob` and `BlobChunk` to improve database design and align with updated requirements.
    
    - Added `Blob` entity to represent file metadata and ownership.
    - Added `BlobChunk` entity to represent chunks of a `Blob`.
    - Updated `CottonDbContext` to include `DbSet<Blob>` and `DbSet<BlobChunk>`.
    - Removed `FileDescriptor` and `FileChunk` entities.
    - Added `Microsoft.EntityFrameworkCore.Tools` package to `Cotton.Server.csproj`.

--- Sources/Cotton.Server/Database/Models/Blob.cs
+++ Sources/Cotton.Server/Database/Models/Blob.cs
@@ -4,15 +4,20 @@ using EasyExtensions.EntityFrameworkCore.Abstractions;
 
 namespace Cotton.Server.Database.Models
 {
-    [Table("file_descriptors")]
+    [Table("blobs")]
+    [Index(nameof(OwnerId), nameof(Parent))]
+    [Index(nameof(OwnerId), nameof(Parent), nameof(Name), IsUnique = true)]
     [Index(nameof(OwnerId), nameof(FileSha256), nameof(SizeBytes), IsUnique = true)]
-    public class FileDescriptor : BaseEntity<Guid>
+    public class Blob : BaseEntity<Guid>
     {
         [Column("owner_id")]
         public Guid OwnerId { get; set; }
 
         [Column("name")]
         public string Name { get; set; } = null!;
+
+        [Column("parent")]
+        public string Parent { get; set; } = null!;
 
         [Column("content_type")]
         public string ContentType { get; set; } = null!;
@@ -23,6 +28,6 @@ namespace Cotton.Server.Database.Models
         [Column("file_sha256", TypeName = "BINARY(32)")]
         public byte[] FileSha256 { get; set; } = null!;
 
-        public ICollection<FileChunk> Chunks { get; set; } = [];
+        public virtual ICollection<BlobChunk> BlobChunks { get; set; } = [];
     }
 }
--- Sources/Cotton.Server/Database/Models/BlobChunk.cs
+++ Sources/Cotton.Server/Database/Models/BlobChunk.cs
@@ -1,23 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
+using EasyExtensions.EntityFrameworkCore.Abstractions;
 
 namespace Cotton.Server.Database.Models
 {
-    [Table("file_chunks")]
+    [Table("blob_chunks")]
     [Index(nameof(ChunkSha256))]
-    [Index(nameof(FileDescriptorId), nameof(Order), IsUnique = true)]
-    public class FileChunk
+    [Index(nameof(BlobId), nameof(Order), IsUnique = true)]
+    public class BlobChunk : BaseEntity<Guid>
     {
-        [Column("file_descriptor_id")]
-        public Guid FileDescriptorId { get; set; }
-
         [Column("chunk_order")]
         public int Order { get; set; } // 0..N-1
+
+        [Column("blob_id")]
+        public Guid BlobId { get; set; }
 
         [Column("chunk_sha256", TypeName = "BINARY(32)")]
         public byte[] ChunkSha256 { get; set; } = null!;
 
         public virtual Chunk Chunk { get; set; } = null!;
-        public virtual FileDescriptor FileDescriptor { get; set; } = null!;
+        public virtual Blob Blob { get; set; } = null!;
     }
 }
