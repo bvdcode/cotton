41710fe430262efdd22c8449214df88c285e2fc3
Vadim Belov <git@belov.us>
2025-10-04 00:41:54 -0700

    Add PostgreSQL support and database integration
    - Added `EasyExtensions.EntityFrameworkCore` and `EasyExtensions.EntityFrameworkCore.Npgsql` NuGet packages.
    - Configured `CottonDbContext` for PostgreSQL in `Program.cs`.
    - Updated `appsettings.Development.json` with database settings.
    - Created `FileDescriptor` entity and integrated it into `CottonDbContext`.
    - Streamlined application pipeline by removing unused middleware.

---
+++ Sources/Cotton.Server/Database/CottonDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cotton.Server.Database.Models;
+using EasyExtensions.EntityFrameworkCore.Database;
+
+namespace Cotton.Server.Database
+{
+    public class CottonDbContext(DbContextOptions options) : AuditedDbContext(options)
+    {
+        public DbSet<FileDescriptor> FileDescriptors => Set<FileDescriptor>();
+    }
+}
---
+++ Sources/Cotton.Server/Database/Models/FileDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EasyExtensions.EntityFrameworkCore.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cotton.Server.Database.Models
+{
+    [Table("file_descriptors")]
+    [Index(nameof(Blake3Hash), IsUnique = true)]
+    public class FileDescriptor : BaseEntity<Guid>
+    {
+        [Column("name")]
+        public string Name { get; set; } = null!;
+
+        [Column("size")]
+        public long Size { get; set; }
+
+        [Column("content_type")]
+        public string ContentType { get; set; } = null!;
+
+        [Column("blake3_hash")]
+        public byte[] Blake3Hash { get; set; } = null!;
+    }
+}
