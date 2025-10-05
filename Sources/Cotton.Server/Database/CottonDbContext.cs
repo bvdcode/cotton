8926209ac8c2517c949efd7c5cbf06be7e8cb77e
Vadim Belov <bvdcode@users.noreply.github.com>
2025-10-05 01:01:12 -0700

    Refactor storage and hashing architecture
    Replaced `Sha256Hasher` and `IHasher` with direct use of `SHA256` for hash validation. Introduced `IStorage` interface and added a placeholder `FileStorage` implementation for chunk storage. Refactored `ChunkController` to use `IStorage` and handle database rollbacks on storage failures. Updated `CottonResult` to provide a more flexible and standardized response model. Removed existing database migrations and added a new `ChunkOwnership` entity to `CottonDbContext`. Cleaned up namespaces and dependencies to reflect the architectural changes.

--- Sources/Cotton.Server/Database/CottonDbContext.cs
+++ Sources/Cotton.Server/Database/CottonDbContext.cs
@@ -6,8 +6,10 @@ namespace Cotton.Server.Database
 {
     public class CottonDbContext(DbContextOptions options) : AuditedDbContext(options)
     {
+        public DbSet<User> Users => Set<User>();
         public DbSet<Blob> Blobs => Set<Blob>();
         public DbSet<Chunk> Chunks => Set<Chunk>();
         public DbSet<BlobChunk> BlobChunks => Set<BlobChunk>();
+        public DbSet<ChunkOwnership> ChunkOwnerships => Set<ChunkOwnership>();
     }
 }
---
+++ Sources/Cotton.Server/Services/FileStorage.cs
@@ -0,0 +1,13 @@
+﻿using Cotton.Server.Settings;
+using Cotton.Server.Abstractions;
+
+namespace Cotton.Server.Services
+{
+    public class FileStorage(CottonSettings _settings) : IStorage
+    {
+        public Task WriteChunkAsync(string hash, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
