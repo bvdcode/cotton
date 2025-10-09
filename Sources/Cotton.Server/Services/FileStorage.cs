b94d1e6e32d7c478a2f89fd901fee3fd46098fd7
Vadim Belov <git@belov.us>
2025-10-09 15:53:55 -0700

    Add stream decryption and improve FileStorage class
    - Updated `IStreamCipher` with `DecryptToStreamAsync`.
    - Implemented `DecryptToStreamAsync` in `AesGcmStreamCipher`.
    - Modified `FileStorage` using directives and changed to partial class.
    - Added `GetChunkReadStream` method for retrieving and decrypting chunks.
    - Refactored `WriteChunkAsync` to use `GetFolderByHash` for path management.
    - Introduced `GetFolderByHash` for hash validation and directory creation.
    - Set file attributes to read-only for written chunks.
    - Improved stream seeking handling for better processing.

--- Sources/Cotton.Server/Services/FileStorage.cs
+++ Sources/Cotton.Server/Services/FileStorage.cs
@@ -1,19 +1,22 @@
-﻿using System.Buffers;
-using Cotton.Server.Settings;
-using Cotton.Server.Abstractions;
+﻿using Cotton.Server.Settings;
 using Cotton.Crypto.Abstractions;
+using Cotton.Server.Abstractions;
+using System.Text.RegularExpressions;
 
 namespace Cotton.Server.Services
 {
-    public class FileStorage : IStorage
+    public partial class FileStorage : IStorage
     {
         private readonly string _basePath;
         private readonly IStreamCipher _cipher;
         private readonly CottonSettings _settings;
         private readonly ILogger<FileStorage> _logger;
 
+        [GeneratedRegex("^[0-9a-f]{64}$", RegexOptions.Compiled)]
+        private static partial Regex CreateHexHashRegex();
         private const string ChunkFileExtension = ".ctn";
         private const string BaseDirectoryName = "chunks";
+
 
         public FileStorage(CottonSettings settings, IStreamCipher cipher, ILogger<FileStorage> logger)
         {
@@ -24,17 +27,36 @@ namespace Cotton.Server.Services
             Directory.CreateDirectory(_basePath);
         }
 
+        public async Task<Stream> GetChunkReadStream(string hash, CancellationToken ct = default)
+        {
+            string dirPath = GetFolderByHash(hash);
+            string filePath = Path.Combine(dirPath, hash[4..] + ChunkFileExtension);
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Chunk not found", filePath);
+            }
+            var fso = new FileStreamOptions
+            {
+                Mode = FileMode.Open,
+                Share = FileShare.Read,
+                Access = FileAccess.Read,
+                Options = FileOptions.Asynchronous | FileOptions.SequentialScan
+            };
+            var fileStream = new FileStream(filePath, fso);
+            var decryptedStream = new MemoryStream(capacity: (int)fileStream.Length);
+            await _cipher.DecryptAsync(fileStream, decryptedStream, ct).ConfigureAwait(false);
+            decryptedStream.Seek(default, SeekOrigin.Begin);
+            await fileStream.DisposeAsync().ConfigureAwait(false);
+            return decryptedStream;
+        }
+
         public async Task WriteChunkAsync(string hash, Stream stream, CancellationToken ct = default)
         {
             if (string.IsNullOrWhiteSpace(hash))
                 throw new ArgumentException("Hash required", nameof(hash));
             ArgumentNullException.ThrowIfNull(stream);
 
-            string p1 = hash[..2];
-            string p2 = hash[2..4];
-            string dirPath = Path.Combine(_basePath, p1, p2);
-            Directory.CreateDirectory(dirPath);
-
+            string dirPath = GetFolderByHash(hash);
             string filePath = Path.Combine(dirPath, hash[4..] + ChunkFileExtension);
             if (File.Exists(filePath))
             {
@@ -52,12 +74,11 @@ namespace Cotton.Server.Services
             };
 
             await using var tmp = new FileStream(tmpFilePath, fso);
-
             try
             {
                 if (stream.CanSeek)
                 {
-                    stream.Seek(0, SeekOrigin.Begin);
+                    stream.Seek(default, SeekOrigin.Begin);
                 }
 
                 await _cipher.EncryptAsync(stream, tmp, _settings.CipherChunkSizeBytes, ct).ConfigureAwait(false);
@@ -79,6 +100,7 @@ namespace Cotton.Server.Services
                 }
 
                 File.Move(tmpFilePath, filePath);
+                File.SetAttributes(filePath, FileAttributes.ReadOnly | FileAttributes.NotContentIndexed);
             }
             catch (IOException)
             {
@@ -95,6 +117,19 @@ namespace Cotton.Server.Services
                 TryDelete(tmpFilePath);
                 throw;
             }
+        }
+
+        private string GetFolderByHash(string hash)
+        {
+            if (string.IsNullOrWhiteSpace(hash) || !CreateHexHashRegex().IsMatch(hash))
+            {
+                throw new ArgumentException("Invalid chunk hash.", nameof(hash));
+            }
+            string p1 = hash[..2];
+            string p2 = hash[2..4];
+            string dirPath = Path.Combine(_basePath, p1, p2);
+            Directory.CreateDirectory(dirPath);
+            return dirPath;
         }
 
         private void TryDelete(string path)
