89728dccf9b288fc89e6e10019650c618374d17f
Vadim Belov <git@belov.us>
2025-10-13 21:49:17 -0700

    Add file management endpoints and update dependencies
    - Added `GetFiles` and enhanced `CreateFileFromChunks` endpoints in `FileController`.
    - Introduced `Mapster` for object mapping and updated `EasyExtensions` dependencies.
    - Added `FileManifestDto` for file manifest data transfer.
    - Enhanced `CreateFileRequest` with additional properties.
    - Added `SizeBytes` property to `Chunk` model.
    - Updated logging configuration for EF Core database commands.
    - Improved project structure with new folder entries.

--- Sources/Cotton.Server/Controllers/FileController.cs
+++ Sources/Cotton.Server/Controllers/FileController.cs
@@ -1,12 +1,25 @@
-﻿using Cotton.Server.Models;
+﻿using Mapster;
+using Cotton.Server.Models;
+using Cotton.Server.Database;
+using Cotton.Server.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Cotton.Server.Models.Requests;
+using Microsoft.EntityFrameworkCore;
+using Cotton.Server.Database.Models;
 
 namespace Cotton.Server.Controllers
 {
     [ApiController]
-    public class FileController : ControllerBase
+    public class FileController(CottonDbContext _dbContext) : ControllerBase
     {
+        [HttpGet(Routes.Files)]
+        public async Task<CottonResult> GetFiles()
+        {
+            var all = await _dbContext.FileManifests.ToListAsync();
+            var mapped = all.Adapt<FileManifestDto[]>();
+            return CottonResult.Ok("Files retrieved successfully.", mapped);
+        }
+
         [HttpGet(Routes.Files + "/{fileId}/download")]
         public async Task<CottonResult> DownloadFile([FromRoute] Guid fileId)
         {
@@ -16,6 +29,20 @@ namespace Cotton.Server.Controllers
         [HttpPost(Routes.Files)]
         public async Task<CottonResult> CreateFileFromChunks([FromBody] CreateFileRequest request)
         {
+            FileManifest newFile = new()
+            {
+                ContentType = request.ContentType,
+                Folder = request.Folder,
+                Name = request.Name,
+                Sha256 = Convert.FromHexString(request.Sha256),
+                SizeBytes = 0
+            };
+            await _dbContext.FileManifests.AddAsync(newFile);
+            await _dbContext.SaveChangesAsync();
+
+
+
+
             return CottonResult.Ok("");
         }
     }
---
+++ Sources/Cotton.Server/Models/Dto/FileManifestDto.cs
@@ -0,0 +1,19 @@
+﻿using EasyExtensions.Models.Dto;
+
+namespace Cotton.Server.Models.Dto
+{
+    public class FileManifestDto : BaseDto<Guid>
+    {
+        public Guid? OwnerId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Folder { get; set; } = null!;
+
+        public string ContentType { get; set; } = null!;
+
+        public long SizeBytes { get; set; }
+
+        public byte[] Sha256 { get; set; } = null!;
+    }
+}
