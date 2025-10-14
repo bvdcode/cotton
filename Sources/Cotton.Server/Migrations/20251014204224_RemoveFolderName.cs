52713ec248975ae6ca844c80584e61699b9cbf4a
Vadim Belov <git@belov.us>
2025-10-14 13:42:35 -0700

    Remove "Folder" property from FileManifest entity
    This commit removes the "Folder" property from the "FileManifest" entity in the database schema. It includes dropping the associated indexes and updating the migration files accordingly. A new migration class `RemoveFolderName` has been created to manage these changes, along with a method to revert them if needed. The `CottonDbContextModelSnapshot` has also been updated to reflect the current model state without the "Folder" property, ensuring consistency in the database context.

---
+++ Sources/Cotton.Server/Migrations/20251014204224_RemoveFolderName.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cotton.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFolderName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_file_manifests_owner_id_folder",
+                table: "file_manifests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_file_manifests_owner_id_folder_name",
+                table: "file_manifests");
+
+            migrationBuilder.DropColumn(
+                name: "folder",
+                table: "file_manifests");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_file_manifests_owner_id",
+                table: "file_manifests",
+                column: "owner_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_file_manifests_owner_id",
+                table: "file_manifests");
+
+            migrationBuilder.AddColumn<string>(
+                name: "folder",
+                table: "file_manifests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_file_manifests_owner_id_folder",
+                table: "file_manifests",
+                columns: new[] { "owner_id", "folder" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_file_manifests_owner_id_folder_name",
+                table: "file_manifests",
+                columns: new[] { "owner_id", "folder", "name" },
+                unique: true);
+        }
+    }
+}
