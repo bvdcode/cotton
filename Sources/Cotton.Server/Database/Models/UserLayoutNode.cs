deed1f434093c7c2e565ff0ce30ef439606a9e85
Vadim Belov <git@belov.us>
2025-10-14 15:51:30 -0700

    Add UserLayout and UserLayoutNode entities
    Updated CottonDbContext to include DbSets for UserLayouts and UserLayoutNodes.
    Modified FileManifest to add VersionStableId and Owner properties.
    Created UserLayout and UserLayoutNode classes with appropriate properties and navigation links.
    Introduced UserLayoutNodeType enumeration for categorization.

---
+++ Sources/Cotton.Server/Database/Models/UserLayoutNode.cs
@@ -0,0 +1,28 @@
+﻿using Cotton.Server.Database.Models.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using EasyExtensions.EntityFrameworkCore.Abstractions;
+
+namespace Cotton.Server.Database.Models
+{
+    [Table("user_layout_nodes")]
+    public class UserLayoutNode : BaseEntity<Guid>
+    {
+        [Column("user_layout_id")]
+        public Guid UserLayoutId { get; set; }
+
+        [Column("parent_id")]
+        public Guid? ParentId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = null!;
+
+        [Column("type")]
+        public UserLayoutNodeType Type { get; set; }
+
+        [Column("is_hidden")]
+        public bool IsHidden { get; set; }
+
+        public virtual UserLayout UserLayout { get; set; } = null!;
+        public virtual UserLayoutNode? Parent { get; set; } = null!;
+    }
+}
