45eea2eb7b8dc1093a1b97770c653b66de790b90
Vadim Belov <git@belov.us>
2025-10-15 10:23:30 -0700

    Add GetChildNodes method and UserLayoutNodeFile entity
    - Implemented GetChildNodes in LayoutController to retrieve child nodes and files for a given parent node.
    - Added DbSet<UserLayoutNodeFile> in CottonDbContext for managing UserLayoutNodeFile entities.
    - Created UserLayoutNodeFile class to represent the relationship between UserLayoutNode and FileManifest.
    - Updated NodeContentDto to include collections for UserLayoutNodeDto and FileManifestDto.

---
+++ Sources/Cotton.Server/Database/Models/UserLayoutNodeFile.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EasyExtensions.EntityFrameworkCore.Abstractions;
+
+namespace Cotton.Server.Database.Models
+{
+    [Table("user_layout_node_files")]
+    public class UserLayoutNodeFile : BaseEntity<Guid>
+    {
+        [Column("file_manifest_id")]
+        public Guid FileManifestId { get; set; }
+
+        [Column("user_layout_node_id")]
+        public Guid UserLayoutNodeId { get; set; }
+
+        public virtual FileManifest FileManifest { get; set; } = null!;
+        public virtual UserLayoutNode UserLayoutNode { get; set; } = null!;
+    }
+}
---
+++ Sources/Cotton.Server/Models/Dto/NodeContentDto.cs
@@ -0,0 +1,10 @@
+﻿using EasyExtensions.Models.Dto;
+
+namespace Cotton.Server.Models.Dto
+{
+    public class NodeContentDto : BaseDto<Guid>
+    {
+        public ICollection<UserLayoutNodeDto> Nodes { get; set; } = [];
+        public ICollection<FileManifestDto> Files { get; set; } = [];
+    }
+}
