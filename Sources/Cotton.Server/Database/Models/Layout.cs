a631ff69ae1acc2825d28ab7814c725e62a6398d
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-10-24 20:47:37 -0700

    Refactor database schema and update related logic
    Replaced `UserLayout` and `UserLayoutNode` with `Layout` and `Node` to simplify and generalize the schema. Updated `CottonDbContext` and navigation properties to reflect these changes.
    
    Created migration `20251025034705_ChangeNames` to rename tables and columns, ensuring data integrity with new indices and constraints. Dropped old tables and updated relationships.
    
    Modified controllers (`FileController`, `LayoutController`) to use the new models. Updated frontend (`FilesPage.tsx`) to align with the new schema, including navigation and translations.
    
    Revised DTOs and snapshots to reflect schema changes. Added new `Layout` and `Node` models with updated properties and relationships. Cleaned up unused imports and adjusted extension methods.

---
+++ Sources/Cotton.Server/Database/Models/Layout.cs
@@ -0,0 +1,11 @@
+﻿using Cotton.Server.Database.Abstractions;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cotton.Server.Database.Models
+{
+    [Table("layouts")]
+    public class Layout : BaseOwnedEntity
+    {
+        public virtual ICollection<Node> Nodes { get; set; } = [];
+    }
+}
--- Sources/Cotton.Server/Models/Dto/UserLayoutNodeDto.cs
+++ Sources/Cotton.Server/Models/Dto/UserLayoutNodeDto.cs
@@ -5,7 +5,7 @@ namespace Cotton.Server.Models.Dto
     public class UserLayoutNodeDto : BaseDto<Guid>
     {
         public Guid LayoutId { get; set; }
-        public Guid? ParentNodeId { get; set; }
+        public Guid? ParentId { get; set; }
         public string Name { get; set; } = null!;
     }
 }
