b1a35008deda2deeaca38b9c3951aa5201b4a9d8
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-10-25 00:27:58 -0700

    Add SPDX license and copyright headers to multiple files; implement license updater script

--- Sources/Cotton.Server/Abstractions/IStorage.cs
+++ Sources/Cotton.Server/Abstractions/IStorage.cs
@@ -1,4 +1,7 @@
-﻿namespace Cotton.Server.Abstractions
+﻿// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2025 Vadim Belov
+
+namespace Cotton.Server.Abstractions
 {
     public interface IStorage
     {
