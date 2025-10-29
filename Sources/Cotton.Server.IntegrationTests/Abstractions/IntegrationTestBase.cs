5e180e7e323cf763e029a6f2c8cbe47a523afc64
Vadim Belov <git@belov.us>
2025-10-28 19:53:09 -0700

    Add integration tests and enhance validation logic
    Introduced a new integration testing framework with the addition of the `Cotton.Server.IntegrationTests` project. This includes setup utilities, database reset functionality, and system information logging. Updated the solution file to include the new project and its build configurations.
    
    Modified `FileController` to compute and validate SHA256 hashes for blob streams, adding a safeguard for data integrity.
    
    Added `IntegrationTestBase` for shared test setup, including PostgreSQL database configuration. Implemented `SystemHelpers` for platform-specific CPU information retrieval. Enhanced testing capabilities to ensure application reliability across environments.

---
+++ Sources/Cotton.Server.IntegrationTests/Abstractions/IntegrationTestBase.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using Cotton.Server.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cotton.Server.IntegrationTests.Abstractions
+{
+    public abstract class IntegrationTestBase : IDisposable
+    {
+        public const string DatabaseName = "cotton_dev_tests";
+        protected CottonDbContext DbContext { get; private set; }
+
+        protected IntegrationTestBase()
+        {
+            const ushort port = 5432;
+            const string host = "localhost";
+            const string username = "postgres";
+            const string password = "postgres";
+
+            DbContextOptionsBuilder<CottonDbContext> optionsBuilder = new();
+            var userBuilder = new NpgsqlConnectionStringBuilder
+            {
+                Host = host,
+                Port = port,
+                Username = username,
+                Password = password,
+                Database = DatabaseName,
+            };
+            optionsBuilder.UseNpgsql(userBuilder.ConnectionString, x =>
+            {
+                x.UseAdminDatabase("postgres");
+            });
+            DbContext = new(optionsBuilder.Options);
+        }
+
+        public void Dispose()
+        {
+            DbContext?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
