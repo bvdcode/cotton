1c193371d9c061b428bf892a03f34d57d2613437
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-15 00:04:31 -0800

    Clear sensitive environment variables after usage
    Enhanced security by clearing sensitive environment variables
    (`COTTON_PG_PASSWORD` and `COTTON_MASTER_KEY`) after they are
    retrieved and used. This prevents sensitive data from lingering
    in the environment and reduces the risk of exposure.

--- Sources/Cotton.Autoconfig/Extensions/ConfigurationBuilderExtensions.cs
+++ Sources/Cotton.Autoconfig/Extensions/ConfigurationBuilderExtensions.cs
@@ -14,12 +14,15 @@ namespace Cotton.Autoconfig.Extensions
             string postgresDb = Environment.GetEnvironmentVariable("COTTON_PG_DATABASE") ?? "cotton_dev";
             string postgresUser = Environment.GetEnvironmentVariable("COTTON_PG_USERNAME") ?? "postgres";
             string postgresPass = Environment.GetEnvironmentVariable("COTTON_PG_PASSWORD") ?? "postgres";
+            Environment.SetEnvironmentVariable("COTTON_PG_PASSWORD", null);
             ushort postgresPort = ushort.Parse(postgresPortStr);
 
             string jwtKey = StringHelpers.CreateRandomString(64);
 
             const int masterKeyId = 1;
             string rootMasterEncryptionKey = Environment.GetEnvironmentVariable("COTTON_MASTER_KEY") ?? "devedovolovopeperepolevopopovedo";
+            Environment.SetEnvironmentVariable("COTTON_MASTER_KEY", null);
+
             string pepper = KeyDerivation.DeriveSubkeyBase64(rootMasterEncryptionKey, "CottonPepper", 16);
             string masterEncryptionKey = KeyDerivation.DeriveSubkeyBase64(rootMasterEncryptionKey, "CottonMasterEncryptionKey", 32);
 
