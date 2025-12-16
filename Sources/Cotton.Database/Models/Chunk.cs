46964121ed2d0764e67f47c9b70b5b0e1302c7bf
Vadim Belov <git@belov.us>
2025-12-15 16:27:36 -0800

    Add compression algorithm support to storage chunks
    Updated NuGet dependencies to latest versions across projects. Added CompressionAlgorithm property to Chunk model and introduced StoragePipelineContext with compression algorithm tracking. These changes enable configuration and tracking of compression algorithms for stored chunks.

--- Sources/Cotton.Database/Models/Chunk.cs
+++ Sources/Cotton.Database/Models/Chunk.cs
@@ -1,6 +1,7 @@
 ﻿// SPDX-License-Identifier: AGPL-3.0-only
 // Copyright (c) 2025 Vadim Belov
 
+using EasyExtensions.Models.Enums;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,5 +16,8 @@ namespace Cotton.Database.Models
 
         [Column("size_bytes")]
         public long SizeBytes { get; set; }
+
+        [Column("compression_algorithm")]
+        public CompressionAlgorithm CompressionAlgorithm { get; set; }
     }
 }
