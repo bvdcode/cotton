ee5ed7db6270d70d5c0e3ea43354f468cfdb4ec7
Vadim Belov <bvdcode@users.noreply.github.com>
2025-12-20 03:04:08 -0800

    Make GetServerSettings public static; add ServerStorageMode enum
    Changed GetServerSettings to public static for wider access.
    Added ServerStorageMode enum with three storage options and
    included license/copyright headers.

---
+++ Sources/Cotton.Database/Models/Enums/ServerStorageMode.cs
@@ -0,0 +1,12 @@
+﻿// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Vadim Belov <https://belov.us>
+
+namespace Cotton.Database.Models.Enums
+{
+    public enum ServerStorageMode
+    {
+        Default = 0,
+        KeepFreeSpace = 1,
+        UseAllAvailableSpace = 2
+    }
+}
