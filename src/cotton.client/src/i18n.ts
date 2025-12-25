0e19e54279c07f0546cdd64facac88bc5e2356fc
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-12-25 01:40:26 -0800

    feat(i18n): refactor localization setup to dynamically load resources and improve language support

--- src/cotton.client/src/i18n.ts
+++ src/cotton.client/src/i18n.ts
@@ -1,27 +1,32 @@
+import {
+  defaultNS,
+  fallbackLng,
+  i18nResources,
+  allNamespaces,
+  supportedLanguages,
+} from "./locales";
 import i18n from "i18next";
-import * as locales from "./locales";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const resources = Object.fromEntries(
-  Object.entries(locales).map(([lng, data]) => [lng, data]),
-);
-const supportedLangs = Object.keys(resources);
+const languageDetectorOptions = {
+  order: ["localStorage", "navigator", "querystring", "cookie", "htmlTag"],
+  lookupFromNavigatorLanguage: true,
+  caches: ["localStorage"],
+  cacheName: "ctn-i18nextLng",
+};
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    fallbackLng: "en",
-    supportedLngs: supportedLangs,
-    nonExplicitSupportedLngs: true,
-    defaultNS: "common",
-    detection: {
-      order: ["localStorage", "navigator", "htmlTag"],
-      caches: ["localStorage"],
-      lookupLocalStorage: "ctn-i18next-lang",
-    },
+    detection: languageDetectorOptions,
+    resources: i18nResources,
+    fallbackLng,
+    supportedLngs: supportedLanguages,
+    ns: allNamespaces,
+    defaultNS,
+    debug: false,
     interpolation: {
       escapeValue: false,
     },
