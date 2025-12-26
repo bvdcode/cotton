7a47d3befd71545ed662b22dfca9b8394dbac176
Vadim Belov <git@belov.us>
2025-12-26 11:29:33 -0800

    Add Benchmark entity and enum to database model
    Added Benchmark entity and BenchmarkType enum for storing and categorizing benchmark results. Registered Benchmarks DbSet in CottonDbContext. Refactored SettingsProvider to use private const fields for default values. Minor cleanup in migration and using statements.

---
+++ src/Cotton.Database/Models/Benchmark.cs
@@ -0,0 +1,24 @@
+﻿using EasyExtensions.EntityFrameworkCore.Abstractions;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cotton.Database.Models
+{
+    [Table("benchmarks")]
+    public class Benchmark : BaseEntity<Guid>
+    {
+        [Column("type")]
+        public BenchmarkType Type { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = null!;
+
+        [Column("value")]
+        public long Value { get; set; }
+
+        [Column("units")]
+        public string Units { get; set; } = null!;
+
+        [Column("elapsed")]
+        public TimeSpan Elapsed { get; set; }
+    }
+}
