8fe5d775d1dec3d78bb17d6be11911f066cbc6f5
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2025-12-26 11:35:44 -0800

    Add Role property to UserDto for user role support
    Added a Role property of type UserRole to the UserDto class to represent user roles in the DTO. Also included the necessary using directive for the UserRole enum.

--- src/Cotton.Server/Models/Dto/UserDto.cs
+++ src/Cotton.Server/Models/Dto/UserDto.cs
@@ -1,6 +1,7 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
+using Cotton.Database.Models.Enums;
 using EasyExtensions.Models.Dto;
 
 namespace Cotton.Server.Models.Dto
@@ -8,5 +9,6 @@ namespace Cotton.Server.Models.Dto
     public class UserDto : BaseDto<Guid>
     {
         public string Username { get; set; } = null!;
+        public UserRole Role { get; set; }
     }
 }
