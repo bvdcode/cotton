fad1d9600594b7110c5010d59b77058e7a3262a6
Vadim Belov <git@belov.us>
2025-12-27 17:14:21 -0800

    Update to EasyExtensions 3.0.20, add Quartz job support
    Upgraded EasyExtensions.* packages to v3.0.20 and replaced the Mediator package with EasyExtensions.Quartz. Integrated Quartz job scheduling in Program.cs and registered jobs with DI. Added CollectPerformanceJob with a daily trigger. Cleaned up using directives in migration files.

---
+++ src/Cotton.Server/Jobs/CollectPerformanceJob.cs
@@ -0,0 +1,14 @@
+﻿using EasyExtensions.Quartz.Attributes;
+using Quartz;
+
+namespace Cotton.Server.Jobs
+{
+    [JobTrigger(days: 1)]
+    public class CollectPerformanceJob(ILogger<CollectPerformanceJob> _logger) : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("CollectPerformanceJob started at {Time}", DateTimeOffset.Now);
+        }
+    }
+}
