673901a94e188b548e81641f3d6568789891e159
Vadim Belov <git@belov.us>
2025-12-30 10:55:07 -0800

    Add DeleteAsync to storage backends and refactor S3 methods
    Introduce DeleteAsync to IStorageBackend and implement it for both FileSystemStorageBackend and S3StorageBackend. Refactor S3StorageBackend's ReadAsync and WriteAsync to use async/await, add argument validation, and set content type and chunk encoding for uploads.

--- src/Cotton.Storage/Backends/S3StorageBackend.cs
+++ src/Cotton.Storage/Backends/S3StorageBackend.cs
@@ -1,18 +1,55 @@
 ﻿using Amazon.S3;
+using Amazon.S3.Model;
 using Cotton.Storage.Abstractions;
+using System.Net;
+using System.Net.Mime;
 
 namespace Cotton.Storage.Backends
 {
     public class S3StorageBackend(IS3Provider _s3Provider) : IStorageBackend
     {
-        public Task<Stream> ReadAsync(string uid)
+        public async Task<bool> DeleteAsync(string uid)
         {
+            ArgumentException.ThrowIfNullOrWhiteSpace(uid);            
             IAmazonS3 _s3 = _s3Provider.GetS3Client();
+            string bucket = _s3Provider.GetBucketName();            
+            var response = await _s3.DeleteObjectAsync(new DeleteObjectRequest
+            {
+                Key = uid,
+                BucketName = bucket
+            });
+            return response.HttpStatusCode == HttpStatusCode.NoContent;
         }
 
-        public Task WriteAsync(string uid, Stream stream)
+        public async Task<Stream> ReadAsync(string uid)
         {
+            ArgumentException.ThrowIfNullOrWhiteSpace(uid);            
             IAmazonS3 _s3 = _s3Provider.GetS3Client();
+            string bucket = _s3Provider.GetBucketName();            
+            var result = await _s3.GetObjectAsync(new GetObjectRequest
+            {
+                Key = uid,
+                BucketName = bucket,
+                ChecksumMode = new ChecksumMode("DISABLED")
+            });
+            return result.ResponseStream;
+        }
+
+        public async Task WriteAsync(string uid, Stream stream)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(uid);
+            ArgumentNullException.ThrowIfNull(stream);
+            IAmazonS3 _s3 = _s3Provider.GetS3Client();
+            string bucket = _s3Provider.GetBucketName();
+            PutObjectRequest req = new()
+            {
+                Key = uid,
+                InputStream = stream,
+                BucketName = bucket,
+                UseChunkEncoding = true,
+                ContentType = MediaTypeNames.Application.Octet,
+            };
+            await _s3.PutObjectAsync(req);
         }
     }
 }
