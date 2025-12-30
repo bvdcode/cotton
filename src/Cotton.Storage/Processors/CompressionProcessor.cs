2a06b6e647dca29bc1b33b97fdb41f2718bd3308
Vadim Belov <git@belov.us>
2025-12-30 11:09:30 -0800

    Add ZstdSharp.Port and prepare for Zstd compression support
    Added ZstdSharp.Port package to enable Zstandard compression.
    Updated CompressionProcessor to reference Zstd and set algorithm to Zstd.
    Removed old BrotliStream code; ReadAsync and WriteAsync now placeholders for future Zstd implementation.

--- src/Cotton.Storage/Processors/CompressionProcessor.cs
+++ src/Cotton.Storage/Processors/CompressionProcessor.cs
@@ -1,24 +1,25 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
+using ZstdSharp;
 using Cotton.Storage.Abstractions;
+using EasyExtensions.Models.Enums;
 
 namespace Cotton.Storage.Processors
 {
     public class CompressionProcessor : IStorageProcessor
     {
+        public const CompressionAlgorithm Algorithm = CompressionAlgorithm.Zstd;
         public int Priority => 10000;
 
         public Task<Stream> ReadAsync(string uid, Stream stream)
         {
-            return Task.FromResult(stream);
-            // return Task.FromResult<Stream>(new BrotliStream(stream, CompressionMode.Decompress, leaveOpen: true));
+
         }
 
         public Task<Stream> WriteAsync(string uid, Stream stream)
         {
-            return Task.FromResult(stream);
-            // return Task.FromResult<Stream>(new BrotliStream(stream, CompressionLevel.Fastest, leaveOpen: true));
+
         }
     }
 }
