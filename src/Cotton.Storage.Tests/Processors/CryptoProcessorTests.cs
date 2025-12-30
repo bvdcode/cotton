c9680ac2c4fd7bf662da9a21e3a88961afcc7bad
Vadim Belov <git@belov.us>
2025-12-30 11:28:22 -0800

    Refactor tests to use real AesGcmStreamCipher
    Replaced all mock IStreamCipher usage in CryptoProcessor and integration tests with real AesGcmStreamCipher instances. Test setup now generates random keys and verifies actual encryption/decryption, including assertions that data on disk is encrypted. Removed all Moq-based helpers and simplified test code to reflect real-world cryptographic behavior. Managed cipher and processor lifecycle in test setup/teardown.

--- src/Cotton.Storage.Tests/Processors/CryptoProcessorTests.cs
+++ src/Cotton.Storage.Tests/Processors/CryptoProcessorTests.cs
@@ -2,9 +2,8 @@
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
 using Cotton.Storage.Processors;
-using EasyExtensions.Abstractions;
+using EasyExtensions.Crypto;
 using NUnit.Framework;
-using Moq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -13,20 +12,34 @@ namespace Cotton.Storage.Tests.Processors
     [TestFixture]
     public class CryptoProcessorTests
     {
+        private AesGcmStreamCipher _cipher = null!;
+        private CryptoProcessor _processor = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            var key = new byte[32];
+            RandomNumberGenerator.Fill(key);
+            _cipher = new AesGcmStreamCipher(key, keyId: 1, threads: null);
+            _processor = new CryptoProcessor(_cipher);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _cipher?.Dispose();
+        }
+
         [Test]
         public async Task CryptoProcessor_RoundTrip_EmptyStream_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = Array.Empty<byte>();
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -38,16 +51,12 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_RoundTrip_OneByte_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = "*"u8.ToArray();
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -59,16 +68,12 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_RoundTrip_SmallData_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = Encoding.UTF8.GetBytes("Hello, World!");
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -80,10 +85,6 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_RoundTrip_1KB_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = new byte[1024];
             for (int i = 0; i < originalData.Length; i++)
             {
@@ -92,8 +93,8 @@ namespace Cotton.Storage.Tests.Processors
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -105,17 +106,13 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_RoundTrip_1MB_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = new byte[1024 * 1024];
             RandomNumberGenerator.Fill(originalData);
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -127,17 +124,13 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_RoundTrip_RandomData_ReturnsOriginal()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupRoundTripCipher(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var originalData = new byte[4096];
             RandomNumberGenerator.Fill(originalData);
             var originalStream = new MemoryStream(originalData);
 
             // Act
-            var encrypted = await processor.WriteAsync("test-uid", originalStream);
-            var decrypted = await processor.ReadAsync("test-uid", encrypted);
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+            var decrypted = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             var result = new MemoryStream();
@@ -146,18 +139,30 @@ namespace Cotton.Storage.Tests.Processors
         }
 
         [Test]
+        public async Task CryptoProcessor_Encrypt_ChangesData()
+        {
+            // Arrange
+            var originalData = Encoding.UTF8.GetBytes("Sensitive Data");
+            var originalStream = new MemoryStream(originalData);
+
+            // Act
+            var encrypted = await _processor.WriteAsync("test-uid", originalStream);
+
+            // Assert
+            var encryptedData = new MemoryStream();
+            await encrypted.CopyToAsync(encryptedData);
+            Assert.That(encryptedData.ToArray(), Is.Not.EqualTo(originalData));
+        }
+
+        [Test]
         public async Task CryptoProcessor_WriteAsync_ReturnsStreamAtPositionZero()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupEncryptMock(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var data = Encoding.UTF8.GetBytes("Test data");
             var stream = new MemoryStream(data);
 
             // Act
-            var result = await processor.WriteAsync("test-uid", stream);
+            var result = await _processor.WriteAsync("test-uid", stream);
 
             // Assert
             Assert.That(result.Position, Is.EqualTo(0));
@@ -167,15 +172,12 @@ namespace Cotton.Storage.Tests.Processors
         public async Task CryptoProcessor_ReadAsync_ReturnsStreamAtPositionZero()
         {
             // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            SetupDecryptMock(mockCipher);
-            
-            var processor = new CryptoProcessor(mockCipher.Object);
             var data = Encoding.UTF8.GetBytes("Test data");
             var stream = new MemoryStream(data);
+            var encrypted = await _processor.WriteAsync("test-uid", stream);
 
             // Act
-            var result = await processor.ReadAsync("test-uid", stream);
+            var result = await _processor.ReadAsync("test-uid", encrypted);
 
             // Assert
             Assert.That(result.Position, Is.EqualTo(0));
@@ -184,56 +186,8 @@ namespace Cotton.Storage.Tests.Processors
         [Test]
         public void CryptoProcessor_Priority_IsCorrect()
         {
-            // Arrange
-            var mockCipher = new Mock<IStreamCipher>();
-            var processor = new CryptoProcessor(mockCipher.Object);
-
             // Assert
-            Assert.That(processor.Priority, Is.EqualTo(1000));
-        }
-
-        private static void SetupRoundTripCipher(Mock<IStreamCipher> mockCipher)
-        {
-            // Simple XOR cipher for testing round-trip
-            mockCipher.Setup(c => c.EncryptAsync(It.IsAny<Stream>()))
-                .ReturnsAsync((Stream s) => XorStream(s, 0xAA));
-            
-            mockCipher.Setup(c => c.DecryptAsync(It.IsAny<Stream>()))
-                .ReturnsAsync((Stream s) => XorStream(s, 0xAA));
-        }
-
-        private static void SetupEncryptMock(Mock<IStreamCipher> mockCipher)
-        {
-            mockCipher.Setup(c => c.EncryptAsync(It.IsAny<Stream>()))
-                .ReturnsAsync((Stream s) => PassthroughStream(s));
-        }
-
-        private static void SetupDecryptMock(Mock<IStreamCipher> mockCipher)
-        {
-            mockCipher.Setup(c => c.DecryptAsync(It.IsAny<Stream>()))
-                .ReturnsAsync((Stream s) => PassthroughStream(s));
-        }
-
-        private static MemoryStream XorStream(Stream input, byte key)
-        {
-            var ms = new MemoryStream();
-            input.CopyTo(ms);
-            var bytes = ms.ToArray();
-            
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                bytes[i] ^= key;
-            }
-            
-            return new MemoryStream(bytes) { Position = 0 };
-        }
-
-        private static MemoryStream PassthroughStream(Stream input)
-        {
-            var ms = new MemoryStream();
-            input.CopyTo(ms);
-            ms.Position = 0;
-            return ms;
+            Assert.That(_processor.Priority, Is.EqualTo(1000));
         }
     }
 }
