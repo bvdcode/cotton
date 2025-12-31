cd6bf14fb71feb7d479067b0bc51c3974591160f
Vadim Belov <bvdcode@users.noreply.github.com>
2025-12-30 23:19:27 -0800

    Add layout stats API endpoint and DTO
    Added a new authorized endpoint to LayoutController for retrieving statistics (size, node count, file count) of a specific user-owned layout. Introduced LayoutStatsDto to represent the returned stats data.

---
+++ src/Cotton.Server/Models/Dto/LayoutStatsDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Cotton.Server.Models.Dto
+{
+    public class LayoutStatsDto
+    {
+        public long SizeBytes { get; init; }
+        public Guid LayoutId { get; init; }
+        public int NodeCount { get; init; }
+        public int FileCount { get; init; }
+    }
+}
