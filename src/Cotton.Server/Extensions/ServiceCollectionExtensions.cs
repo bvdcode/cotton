2da3a4f0b43aed50f48a84f20446684bd06451a9
Vadim Belov <bvdcode@users.noreply.github.com>
2026-01-01 21:04:52 -0800

    Replace MemoryCache with ConcurrentDictionary for caching
    Refactored CachedStoragePipeline to use ConcurrentDictionary for
    in-memory caching instead of System.Runtime.Caching.MemoryCache.
    Implemented cache eviction and logging. Updated DI to use
    CachedStoragePipeline for IStoragePipeline. Removed Mapster
    usings and System.Runtime.Caching package reference.

--- src/Cotton.Server/Extensions/ServiceCollectionExtensions.cs
+++ src/Cotton.Server/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,9 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
-using Cotton.Server.Services;
 using Cotton.Shared;
 using EasyExtensions.Abstractions;
 using EasyExtensions.Crypto;
-using System.Text;
 
 namespace Cotton.Server.Extensions
 {
