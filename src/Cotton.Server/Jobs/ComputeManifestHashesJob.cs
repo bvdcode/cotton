811f681fbae291e3dade3f9a676f5eac21fea3e1
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-01-02 00:07:21 -0800

    Add ComputeManifestHashesJob with hourly Quartz trigger
    Introduced ComputeManifestHashesJob in Cotton.Server.Jobs, implementing Quartz's IJob interface. The job is scheduled to run every hour using the [JobTrigger(hours: 1)] attribute. The Execute method currently contains a placeholder for manifest hash computation logic.

---
+++ src/Cotton.Server/Jobs/ComputeManifestHashesJob.cs
@@ -0,0 +1,15 @@
+﻿using EasyExtensions.Quartz.Attributes;
+using Quartz;
+
+namespace Cotton.Server.Jobs
+{
+    [JobTrigger(hours: 1)]
+    public class ComputeManifestHashesJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            // TODO: Implement manifest hash computation logic here
+            return Task.CompletedTask;
+        }
+    }
+}
