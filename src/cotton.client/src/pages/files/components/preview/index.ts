c2992a420882eff6b1f669e328c338ef5aca75ce
Vadim Belov <git@belov.us>
2026-01-18 01:33:22 -0800

    refactor: remove VideoPreview component from preview exports

--- src/cotton.client/src/pages/files/components/preview/index.ts
+++ src/cotton.client/src/pages/files/components/preview/index.ts
@@ -1,4 +1,3 @@
 export { PreviewModal } from "./PreviewModal";
 export { PdfPreview } from "./PdfPreview";
-export { VideoPreview } from "./VideoPreview";
 export { TextPreview } from "./TextPreview";
