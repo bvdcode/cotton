595cc05929fe2096ba54c3c85ec5b098afd9ddfa
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-01-17 17:02:21 -0800

    Add InterfaceLayoutType enum and update Node model with layout type property

---
+++ src/Cotton.Database/Migrations/20260118005931_AddNodeInterfaceLayoutType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cotton.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNodeInterfaceLayoutType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ui_layout_type",
+                table: "nodes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ui_layout_type",
+                table: "nodes");
+        }
+    }
+}
