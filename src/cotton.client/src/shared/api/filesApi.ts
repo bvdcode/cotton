ece942ba8ff5105301c2a4ccd02a70180d2767fb
Vadim Belov <git@belov.us>
2026-01-18 01:03:22 -0800

    Add baseManifestId to CreateFileFromChunksRequest interface

--- src/cotton.client/src/shared/api/filesApi.ts
+++ src/cotton.client/src/shared/api/filesApi.ts
@@ -8,6 +8,7 @@ export interface CreateFileFromChunksRequest {
   contentType: string;
   hash: string | null;
   originalNodeFileId?: Guid | null;
+  baseManifestId?: Guid | null;
 }
 
 export interface RenameFileRequest {
