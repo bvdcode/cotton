0fe1e5c6fbd4c7eb403252d6a626f25905d52529
Vadim Belov <git@belov.us>
2026-01-19 15:35:17 -0800

    Clean up and reorder using directives across codebase
    Removed unused usings and reordered imports for consistency.
    Fixed namespace declarations in some files. Added clarifying
    comments and minor whitespace tweaks in FileController.cs.
    No functional changes.

--- src/Cotton.Database/Models/User.cs
+++ src/Cotton.Database/Models/User.cs
@@ -1,7 +1,6 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
-using Cotton.Database.Models.Enums;
 using EasyExtensions.EntityFrameworkCore.Abstractions;
 using EasyExtensions.Models.Enums;
 using Microsoft.EntityFrameworkCore;
