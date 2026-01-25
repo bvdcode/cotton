fabedb86fd28237688df515cb443fda7f30cb8eb
Vadim Belov <git@belov.us>
2026-01-25 03:43:04 -0800

    Refactor SearchBar and useSearch hooks for improved debounce handling and cleanup

--- src/Cotton.Server/Models/Dto/SearchResultDto.cs
+++ src/Cotton.Server/Models/Dto/SearchResultDto.cs
@@ -7,8 +7,5 @@ namespace Cotton.Server.Models.Dto
     {
         public IEnumerable<NodeDto> Nodes { get; set; } = [];
         public IEnumerable<FileManifestDto> Files { get; set; } = [];
-        public int TotalCount { get; set; }
-        public int Page { get; set; }
-        public int PageSize { get; set; }
     }
 }
