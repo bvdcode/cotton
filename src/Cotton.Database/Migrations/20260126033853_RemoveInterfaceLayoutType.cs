81a4960218bb98ba0befaecd26f64178272d5b5a
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-01-25 19:39:04 -0800

    Remove InterfaceLayoutType column from nodes table; update name_key column type to citext for improved performance and consistency

---
+++ src/Cotton.Database/Migrations/20260126033853_RemoveInterfaceLayoutType.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cotton.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveInterfaceLayoutType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ui_layout_type",
+                table: "nodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_key",
+                table: "nodes",
+                type: "citext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_key",
+                table: "node_files",
+                type: "citext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name_key",
+                table: "nodes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "citext");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ui_layout_type",
+                table: "nodes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name_key",
+                table: "node_files",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "citext");
+        }
+    }
+}
