52c0b8d90510407319e23392ec94b5f6a9c47fce
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-02-02 21:44:17 -0800

    Add HEIC/HEIF preview support using ImageSharp.Heif
    Introduced HeicPreviewGenerator to enable preview generation for HEIC/HEIF images. Integrated HeyRed.ImageSharp.Heif and LibHeif.Native packages, and registered the new generator in PreviewGeneratorProvider. Previews are generated as WebP images.

---
+++ src/Cotton.Previews/HeicPreviewGenerator.cs
@@ -0,0 +1,32 @@
+﻿using HeyRed.ImageSharp.Heif;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Cotton.Previews
+{
+    public class HeicPreviewGenerator : IPreviewGenerator
+    {
+        public IEnumerable<string> SupportedContentTypes =>
+        [
+            "image/heic",
+            "image/heic-sequence",
+            "image/heif",
+            "image/heif-sequence"
+        ];
+
+        public async Task<byte[]> GeneratePreviewWebPAsync(Stream stream, int size = PreviewGeneratorProvider.DefaultPreviewSize)
+        {
+            var options = new HeifDecoderOptions();
+            using Image<Rgba32> image = HeifDecoder.Instance.Decode<Rgba32>(options, stream);
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(size, size),
+                Mode = ResizeMode.Max
+            }));
+            using var outputStream = new MemoryStream();
+            await image.SaveAsWebpAsync(outputStream);
+            return outputStream.ToArray();
+        }
+    }
+}
