fa9c224051d323dd3329db670d6ca2bdd77157fd
Vadim Belov <git@belov.us>
2026-02-10 23:13:56 -0800

    Add "Trust Device" support to login and update dependencies
    Refactor login to use new LoginRequest model with TrustDevice flag, enabling longer session duration for trusted devices. Update refresh token and cookie logic accordingly. Bump various NuGet package versions across projects.

---
+++ src/Cotton.Server/Models/Dto/LoginRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cotton.Server.Models.Dto
+{
+    public class LoginRequest
+    {
+        /// <summary>
+        /// Gets or sets the username associated with the user account.
+        /// </summary>
+        [Required]
+        public string Username { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the password used for authentication.
+        /// </summary>
+        [Required]
+        public string Password { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the two-factor authentication code provided by the user.
+        /// </summary>
+        public string? TwoFactorCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the current device should be trusted for future authentication
+        /// attempts.
+        /// </summary>
+        public bool TrustDevice { get; set; }
+    }
+}
