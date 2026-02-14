4ebd79f143ecc3691d54d4249fdc937edb5d0ef9
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-02-13 22:50:48 -0800

    Add non-nullable hstore preferences to users table
    Added a new non-nullable preferences column of type hstore to the users table for storing user preferences as key-value pairs. Updated EF Core migration, model snapshot, and designer files to support this schema change.

---
+++ src/Cotton.Database/Migrations/20260214065038_AddUserPreferences.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cotton.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Dictionary<string, string>>(
+                name: "preferences",
+                table: "users",
+                type: "hstore",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "preferences",
+                table: "users");
+        }
+    }
+}
