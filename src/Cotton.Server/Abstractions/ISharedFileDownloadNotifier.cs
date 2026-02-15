9b85457b06bd0defb7f5926b2d37cc7dbc9cb440
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-02-15 02:46:50 -0800

    Refactor Dockerfile and remove unused usings
    Modernize Dockerfile with multi-stage build, Node.js 25.x install, and NuGet cache for faster builds. Remove unused using directives from several C# files to clean up dependencies. Also remove redundant using in migration file.

--- src/Cotton.Server/Abstractions/ISharedFileDownloadNotifier.cs
+++ src/Cotton.Server/Abstractions/ISharedFileDownloadNotifier.cs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
-using Microsoft.AspNetCore.Http;
-
 namespace Cotton.Server.Abstractions
 {
     public interface ISharedFileDownloadNotifier
