2ae6aeffafbef286914ed1460da348c973eb8430
Vadim Belov <git@belov.us>
2026-02-21 23:51:35 -0800

    refactor: simplify reduction percent calculation and clean up unused comments in service interfaces

--- src/Cotton.Server/Abstractions/IChunkIngestService.cs
+++ src/Cotton.Server/Abstractions/IChunkIngestService.cs
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Vadim Belov <https://belov.us>
 
-
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2025 Vadim Belov <https://belov.us>
-
 using Cotton.Database.Models;
 
 namespace Cotton.Server.Abstractions;
