9e1f1f20cc6c91d86d56cab64098cb0895d92853
Vadim Belov <git@belov.us>
2026-02-25 10:23:25 -0800

    Add EncryptedFileLargePreviewHash to file_manifests
    Introduce a new nullable column, encrypted_file_large_preview_hash (bytea), to the file_manifests table via a new EF Core migration. Update the model snapshot and migration designer to include the new property in the FileManifest entity. This supports storing a hash for large encrypted file previews.

---
+++ src/Cotton.Database/Migrations/20260225182243_AddEncryptedFileLargePreviewHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cotton.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEncryptedFileLargePreviewHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "encrypted_file_large_preview_hash",
+                table: "file_manifests",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "encrypted_file_large_preview_hash",
+                table: "file_manifests");
+        }
+    }
+}
