40e21ecd78d43fc22a7ab6bdec00b87495df7503
Vadim Belov <git@belov.us>
2026-03-04 09:11:09 -0800

    feat: enhance file selection handling with long press support and refactor layout components

--- src/cotton.client/src/app/layouts/PublicLayout.tsx
+++ src/cotton.client/src/app/layouts/PublicLayout.tsx
@@ -1,17 +1,21 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 export const PublicLayout = () => {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      flexDirection="column"
+      alignItems="stretch"
+      justifyContent="flex-start"
+      minHeight={0}
+      minWidth={0}
     >
-      <Outlet />
-    </div>
+      <Box flex={1} minHeight={0} minWidth={0} overflow="auto">
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
