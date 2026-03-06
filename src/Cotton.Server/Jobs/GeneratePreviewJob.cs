df00561ff6090a111ca3bc148c268f5edeea555f
Vadim Belov <bvdcode@users.noreply.github.com>
2026-03-05 19:41:14 -0800

    Restrict large preview generation to image generators
    Removed VideoPreviewGenerator from condition that triggers large preview image generation; now only ImagePreviewGenerator and HeicPreviewGenerator will generate large previews.

--- src/Cotton.Server/Jobs/GeneratePreviewJob.cs
+++ src/Cotton.Server/Jobs/GeneratePreviewJob.cs
@@ -81,9 +81,7 @@ namespace Cotton.Server.Jobs
                     item.SmallFilePreviewHash = hash;
                     item.SmallFilePreviewHashEncrypted = _crypto.Encrypt(hash);
 
-                    if (generator is ImagePreviewGenerator
-                        or HeicPreviewGenerator
-                        or VideoPreviewGenerator)
+                    if (generator is ImagePreviewGenerator or HeicPreviewGenerator)
                     {
                         await using var fsLarge = _storage.GetBlobStream(uids, pipelineContext);
                         byte[] previewImageLarge = await generator.GeneratePreviewWebPAsync(fsLarge, PreviewGeneratorProvider.DefaultLargePreviewSize);
