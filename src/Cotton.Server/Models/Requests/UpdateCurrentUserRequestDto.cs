399ba6a36e56dd6b6ebad79d33dc4cdc8a979518
Vadim Belov <git@belov.us>
2026-04-21 19:40:44 -0700

    Add support for updating username in user profile
    Enable updating the username field as part of the user profile update flow. This includes adding a Username property to the update request DTO, updating the command to accept and store the username, and passing the username from the controller to the command.

--- src/Cotton.Server/Models/Requests/UpdateCurrentUserRequestDto.cs
+++ src/Cotton.Server/Models/Requests/UpdateCurrentUserRequestDto.cs
@@ -6,6 +6,7 @@ namespace Cotton.Server.Models.Requests
     public class UpdateCurrentUserRequestDto
     {
         public string? Email { get; set; }
+        public string? Username { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public DateOnly? BirthDate { get; set; }
