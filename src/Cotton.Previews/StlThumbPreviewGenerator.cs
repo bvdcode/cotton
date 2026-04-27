e2886e35de28f10207bb6c050891db0920e6bdd3
Vadim Belov <git@belov.us>
2026-04-27 09:20:02 -0700

    Add UTF-8 marshalling for modelFilename parameter in StlThumbPreviewGenerator

--- src/Cotton.Previews/StlThumbPreviewGenerator.cs
+++ src/Cotton.Previews/StlThumbPreviewGenerator.cs
@@ -408,7 +408,7 @@ namespace Cotton.Previews
                 byte[] buffer,
                 uint width,
                 uint height,
-                [MarshalAs(UnmanagedType.LPWStr)] string modelFilename);
+                [MarshalAs(UnmanagedType.LPUTF8Str)] string modelFilename);
 #pragma warning restore SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
         }
     }
