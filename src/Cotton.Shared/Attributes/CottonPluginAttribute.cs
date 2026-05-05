14aa0a800649501594abbb2f640f9364edfeb834
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-05-05 16:52:08 -0700

    Add

---
+++ src/Cotton.Shared/Attributes/CottonPluginAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cotton.Attributes
+{
+    /// <summary>
+    /// Indicates that a class is a plugin for the Cotton Cloud.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class CottonPluginAttribute : Attribute
+    {
+        public string PluginId { get; }
+
+        public CottonPluginAttribute(string pluginId)
+        {
+            PluginId = pluginId;
+        }
+    }
+}
