e0920960b8f4ba298bb88c68d88318a71ed512e8
Vadim Belov <git@belov.us>
2026-05-05 16:52:08 -0700

    Add

---
+++ src/Cotton.Shared/Abstractions/ICottonPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cotton.Abstractions
+{
+    /// <summary>
+    /// Represents a plugin for the Cotton Cloud.
+    /// </summary>
+    public interface ICottonPlugin
+    {
+
+    }
+}
