8d9bc4ee735739f58bc9e0acb2df15ddbef7d73b
Vadim Belov <81349914+bvdcode@users.noreply.github.com>
2026-05-22 06:46:30 +0000

    chore(deps): refresh .NET package patch versions

--- src/Cotton.Previews/PreviewColorPalette.cs
+++ src/Cotton.Previews/PreviewColorPalette.cs
@@ -6,6 +6,7 @@ namespace Cotton.Previews
     internal static class PreviewColorPalette
     {
         public const string AccentGreenHex = "#96BE02";
+        public const string AccentGreenF3dRgb = "0.588235,0.745098,0.007843";
         public const byte AccentGreenRed = 0x96;
         public const byte AccentGreenGreen = 0xBE;
         public const byte AccentGreenBlue = 0x02;
